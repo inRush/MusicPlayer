20226c52227ac7479abab085c5830eb24f21cbd6
inRush <in_rush@outlook.com>
2018-01-12 12:19:24 +0800

    用AndroidUtilsCode里的PermissionUtils替换EasyPermission,添加更加完善的权限获取逻辑

--- app/src/main/java/me/inrush/mediaplayer/MainActivity.java
+++ app/src/main/java/me/inrush/mediaplayer/MainActivity.java
@@ -1,8 +1,6 @@
 package me.inrush.mediaplayer;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 
 import java.util.List;
@@ -19,18 +20,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.inrush.mediaplayer.fragments.VideoFragment;
-import me.inrush.mediaplayer.media.music.pages.MusicFragment;
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.AppSettingsDialog;
-import pub.devrel.easypermissions.EasyPermissions;
+import me.inrush.mediaplayer.helper.DialogHelper;
+import me.inrush.mediaplayer.media.music.pages.musiclist.MusicFragment;
 
 /**
  * @author inrush
  */
-public class MainActivity extends AppCompatActivity
-        implements EasyPermissions.PermissionCallbacks {
-
-    private static final int RC = 0x8888;
+public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_music)
     TextView mMusicPageBtn;
@@ -38,19 +34,36 @@ public class MainActivity extends AppCompatActivity
     TextView mVideoPageBtn;
     @BindView(R.id.vp_content)
     ViewPager mContent;
-
+    private String[] perms = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private boolean isInit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         QMUIStatusBarHelper.translucent(this);
         setContentView(R.layout.activity_main);
-        // 申请权限
-        if (!havePerm(this)) {
-            requestPerm();
-        }
         ButterKnife.bind(this);
-        init();
+        selectMusicPage();
+    }
+
+    @Override
+    protected void onStart() {
+        if(checkPermissions() && !isInit){
+            init();
+            isInit = true;
+        }
+        super.onStart();
+    }
+
+    private boolean checkPermissions() {
+        if (!PermissionUtils.isGranted(perms)) {
+            requestPerm();
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -71,7 +84,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void init() {
-        selectMusicPage();
         mContent.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
@@ -132,61 +144,36 @@ public class MainActivity extends AppCompatActivity
     }
 
     /**
-     * 获取是否有读权限
-     *
-     * @param context 上下文
-     * @return true就是有
-     */
-    private static boolean havePerm(Context context) {
-        // 准备需要检查的录音权限
-        String[] perms = new String[]{
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-
-        return EasyPermissions.hasPermissions(context, perms);
-    }
-
-
-    /**
      * 申请权限
      */
-    @AfterPermissionGranted(RC)
     private void requestPerm() {
-        String[] perms = new String[]{
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-        if (EasyPermissions.hasPermissions(this, perms)) {
-            App.showToast("获取权限成功");
-        } else {
-            EasyPermissions.requestPermissions(this, "授予权限", RC, perms);
-        }
-    }
+        PermissionUtils.permission(perms)
+                .rationale(new PermissionUtils.OnRationaleListener() {
+                    @Override
+                    public void rationale(ShouldRequest shouldRequest) {
+                        DialogHelper.showRationaleDialog(shouldRequest);
+                    }
+                })
+                .callback(new PermissionUtils.FullCallback() {
+                    @Override
+                    public void onGranted(List<String> permissionsGranted) {
+                        if(!isInit){
+                            isInit = true;
+                            init();
+                        }
+                        ToastUtils.showShort(R.string.get_permissions_successful);
+                    }
 
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-        // 如果有没有申请成功的权限存在,则弹出弹出框,用户点击去到设置界面自己打开权限
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            new AppSettingsDialog
-                    .Builder(this)
-                    .build().show();
-        }
+                    @Override
+                    public void onDenied(List<String> permissionsDeniedForever, List<String> permissionsDenied) {
+                        DialogHelper.showOpenAppSettingDialog();
+                    }
+                }).request();
     }
 
     @Override
     public void onBackPressed() {
-        goHome();
+        ActivityUtils.startHomeActivity();
     }
 
-    private void goHome() {
-        Intent home = new Intent(Intent.ACTION_MAIN);
-        home.addCategory(Intent.CATEGORY_HOME);
-        startActivity(home);
-    }
 }
